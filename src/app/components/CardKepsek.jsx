c556e2996e236ddf91c5bba0ac3dc6d3ea2ce6c7
Consep33t <agengp360@gmail.com>
2025-02-07 17:52:57 +0700

    first commit

---
+++ src/app/components/CardKepsek.jsx
@@ -0,0 +1,20 @@
+const CardKepsek = () => {
+  return (
+    <div className="card bg-base-100 w-96 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+          className="rounded-xl h-96"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CardKepsek;
---
+++ src/app/profile/page.jsx
@@ -0,0 +1,8 @@
+const profile = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1>Profile</h1>
+    </div>
+  );
+};
+export default profile;
